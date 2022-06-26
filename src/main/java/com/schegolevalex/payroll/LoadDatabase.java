38127b4a9f251903581e7adbca1260f46d95c721
Alexey Schegolev <schegolevalex@gmail.com>
2022-06-26 17:53:56 +0300

    add new classes to the project

--- src/main/java/com/schegolevalex/payroll/LoadDatabase.java
+++ src/main/java/com/schegolevalex/payroll/LoadDatabase.java
@@ -13,7 +13,6 @@ class LoadDatabase {
 
     @Bean
     CommandLineRunner initDatabase(EmployeeRepository repository) {
-
         return args -> {
             log.info("Preloading " + repository.save(new Employee("Bilbo Baggins", "burglar")));
             log.info("Preloading " + repository.save(new Employee("Frodo Baggins", "thief")));
