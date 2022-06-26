85d7fa3b01981aaa364c5e032b3d158913bf80be
Alexey Schegolev <schegolevalex@gmail.com>
2022-06-26 15:13:14 +0300

    first commit

---
+++ src/test/java/com/schegolevalex/payroll/PayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schegolevalex.payroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
