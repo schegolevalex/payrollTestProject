ea4de5be0d5ad7dfc68f477485f2c5b0c1ebf5cd
schegolevalex <schegolevalex@gmail.com>
2024-01-12 09:52:37 +0300

    controllers refactor

--- src/main/java/com/schegolevalex/payroll/controllers/EmployeeController.java
+++ src/main/java/com/schegolevalex/payroll/controllers/EmployeeController.java
@@ -1,9 +1,10 @@
 package com.schegolevalex.payroll.controllers;
 
 import com.schegolevalex.payroll.assemblers.EmployeeModelAssembler;
-import com.schegolevalex.payroll.exceptions.EmployeeNotFoundException;
 import com.schegolevalex.payroll.entities.Employee;
+import com.schegolevalex.payroll.exceptions.EmployeeNotFoundException;
 import com.schegolevalex.payroll.repositories.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.IanaLinkRelations;
@@ -17,33 +18,23 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
+@RequiredArgsConstructor
 public class EmployeeController {
 
     private final EmployeeRepository repository;
-
     private final EmployeeModelAssembler assembler;
-
-    public EmployeeController(EmployeeRepository repository, EmployeeModelAssembler assembler) {
-
-        this.repository = repository;
-        this.assembler = assembler;
-    }
 
     @GetMapping("/employees")
     public CollectionModel<EntityModel<Employee>> all() {
-
         List<EntityModel<Employee>> employees = repository.findAll().stream()
                 .map(assembler::toModel)
                 .collect(Collectors.toList());
-
         return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
     }
 
     @PostMapping("/employees")
     public ResponseEntity<?> newEmployee(@RequestBody Employee newEmployee) {
-
         EntityModel<Employee> entityModel = assembler.toModel(repository.save(newEmployee));
-
         return ResponseEntity
                 .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
                 .body(entityModel);
@@ -53,13 +44,11 @@ public class EmployeeController {
     public EntityModel<Employee> one(@PathVariable Long id) {
         Employee employee = repository.findById(id)
                 .orElseThrow(() -> new EmployeeNotFoundException(id));
-
         return assembler.toModel(employee);
     }
 
     @PutMapping("/employees/{id}")
     public ResponseEntity<?> replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
-
         Employee updatedEmployee = repository.findById(id)
                 .map(employee -> {
                     employee.setName(newEmployee.getName());
@@ -72,7 +61,6 @@ public class EmployeeController {
                 });
 
         EntityModel<Employee> entityModel = assembler.toModel(updatedEmployee);
-
         return ResponseEntity
                 .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
                 .body(entityModel);
@@ -80,9 +68,7 @@ public class EmployeeController {
 
     @DeleteMapping("/employees/{id}")
     public ResponseEntity<?> deleteEmployee(@PathVariable Long id) {
-
         repository.deleteById(id);
-
         return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/schegolevalex/payroll/controllers/OrderController.java
+++ src/main/java/com/schegolevalex/payroll/controllers/OrderController.java
@@ -1,13 +1,13 @@
 package com.schegolevalex.payroll.controllers;
 
 import com.schegolevalex.payroll.Status;
+import com.schegolevalex.payroll.assemblers.OrderModelAssembler;
 import com.schegolevalex.payroll.entities.Order;
 import com.schegolevalex.payroll.exceptions.OrderNotFoundException;
 import com.schegolevalex.payroll.repositories.OrderRepository;
-import com.schegolevalex.payroll.assemblers.OrderModelAssembler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.mediatype.problem.Problem;
 import org.springframework.http.HttpHeaders;
@@ -18,17 +18,14 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
+@RequiredArgsConstructor
 public class OrderController {
-    private OrderRepository orderRepository;
-    private OrderModelAssembler orderAssembler;
-
-    public OrderController(OrderRepository orderRepository, OrderModelAssembler orderAssembler) {
-        this.orderRepository = orderRepository;
-        this.orderAssembler = orderAssembler;
-    }
+    private final OrderRepository orderRepository;
+    private final OrderModelAssembler orderAssembler;
 
     @GetMapping("/orders")
     public CollectionModel<EntityModel<Order>> all() {
@@ -47,20 +44,16 @@ public class OrderController {
     @PostMapping("/orders")
     public ResponseEntity<EntityModel<Order>> newOrder(@RequestBody Order order) {
         order.setStatus(Status.IN_PROGRESS);
-
         EntityModel<Order> entityModel = orderAssembler.toModel(orderRepository.save(order));
         return ResponseEntity
                 .created(linkTo(methodOn(OrderController.class).one(order.getId())).toUri())
-//                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
                 .body(entityModel);
     }
 
     @DeleteMapping("/orders/{id}/cancel")
     public ResponseEntity<?> cancel(@PathVariable Long id) {
-
         Order order = orderRepository.findById(id)
                 .orElseThrow(() -> new OrderNotFoundException(id));
-
         if (order.getStatus() == Status.IN_PROGRESS) {
             order.setStatus(Status.CANCELLED);
             return ResponseEntity.ok(orderAssembler.toModel(orderRepository.save(order)));
@@ -77,20 +70,17 @@ public class OrderController {
     @PutMapping("/orders/{id}/complete")
     public ResponseEntity<?> complete(@PathVariable Long id) {
 
-        Order order = orderRepository.findById(id) //
-                .orElseThrow(() -> new OrderNotFoundException(id));
-
+        Order order = orderRepository.findById(id).orElseThrow(() -> new OrderNotFoundException(id));
         if (order.getStatus() == Status.IN_PROGRESS) {
             order.setStatus(Status.COMPLETED);
             return ResponseEntity.ok(orderAssembler.toModel(orderRepository.save(order)));
         }
 
-        return ResponseEntity //
-                .status(HttpStatus.METHOD_NOT_ALLOWED) //
-                .header(HttpHeaders.CONTENT_TYPE, MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE) //
-                .body(Problem.create() //
-                        .withTitle("Method not allowed") //
+        return ResponseEntity
+                .status(HttpStatus.METHOD_NOT_ALLOWED)
+                .header(HttpHeaders.CONTENT_TYPE, MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE)
+                .body(Problem.create()
+                        .withTitle("Method not allowed")
                         .withDetail("You can't complete an order that is in the " + order.getStatus() + " status"));
     }
-
 }
